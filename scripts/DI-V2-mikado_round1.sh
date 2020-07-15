61bce26c23eda3753d4315fcdedbe71eb69eca1e
lijing28101 <jingli@iastate.edu>
2020-07-15 14:50:17 -0500

    Update DI-V2-mikado_round1.sh

--- scripts/DI-V2-mikado_round1.sh
+++ scripts/DI-V2-mikado_round1.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=96
+#SBATCH -t 96:00:00
+#SBATCH -J prepare
+#SBATCH -o prepare.o%j
+#SBATCH -e prepare.e%j
+
+
+source ~/.bashrc
+source /path/to/Anaconda3/etc/profile.d/conda.sh
+conda activate mikado2
+
 mikado configure \
    --list list.txt \
    --reference Zm-B73-REFERENCE-NAM-5.0.fa \
