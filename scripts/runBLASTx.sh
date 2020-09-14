2a2f436b63abd26631e570788bc8378c08cbab25
Jacqueline Campbell <jdjax@iastate.edu>
2020-09-14 15:48:10 -0500

    Update runBLASTx.sh

--- scripts/runBLASTx.sh
+++ scripts/runBLASTx.sh
@@ -1,3 +1,25 @@
+##BLASTx using Uniprot-Swiss
+#!/bin/bash
+transcripts=$1
+ml load ncbi-blast
+target=uniprot_sprot.fasta
+
+makeblastdb \
+   -in $target \
+   -dbtype prot \
+   -parse_seqids
+
+blastx  \
+	-max_target_seqs 5 \
+	-num_threads 28 \
+	-query ${transcripts}
+	-outfmt 5 \
+	-db $target \
+	-evalue 0.000001 2> blast.log | sed '/^$/d' > mikado.blast.xml
+
+
+
+##BLASTn using the genome
 #!/bin/bash
 transcripts=$1
 ml load ncbi-blast
