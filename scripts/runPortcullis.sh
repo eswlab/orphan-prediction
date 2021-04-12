f47cb5ed2a91c0716a9485626e5d052444f0cd98
lijing28101 <jingli@iastate.edu>
2021-04-12 13:14:04 -0500

    Delete runPortcullis.sh

--- scripts/runPortcullis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-module purge
-source ~/.bashrc
-source activate
-ref=/work/GIF/arnstrm/GENOMEDB/TAIR10_chr_all.fas
-bam=/work/LAS/mash-lab/arnstrm/20180727_Mikado/01_alignment/merged_better_and_best.bam
-portcullis full --threads 16 --verbose --use_csi --output portcullis_out --orientation FR $ref $bam
